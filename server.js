c57923263ee92a38ecb997b022ad22050f9bd683
Trenity Gilford <trenity@Trenitys-MacBook-Pro.local>
2025-09-14 19:31:35 -0400

    Added server, Axios, and ChartJS

--- server.js
+++ server.js
@@ -4,26 +4,58 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = 3000;
+const budget = require('./budget.json');
+app.use('/', express.static('public'));
 
-app.use(cors());
+// const budget = {
+//     myBudget: [
+//         {
+//             title: 'Eat out',
+//             budget: 25
+//         },
+//         {
+//             title: 'Rent',
+//             budget: 275
+//         },
+//         {
+//             title: 'Groceries',
+//             budget: 110
+//         },
+//         {
+//             title: 'Utilities',
+//             budget: 150
+//         },
+//         {
+//             title: 'Phone',
+//             budget: 45
+//         },
+//         {
+//             title: 'Car',
+//             budget: 150
+//         },
+//         {
+//             title: 'Gas',
+//             budget: 65
+//         },
+//         {
+//             title: 'Savings',
+//             budget: 150
+//         },
+//         {
+//             title: 'Netflix Subscription',
+//             budget: 90
+//         },
+//         {
+//             title: 'Eating out fund',
+//             budget: 200
+//         },
+//     ]
+// };
 
-const budget = {
-    myBudget: [
-        {
-            title: 'Eat out',
-            budget: 25
-        },
-        {
-            title: 'Rent',
-            budget: 275
-        },
-        {
-            title: 'Grocery',
-            budget: 110
-        },
-    ]
-};
 
+app.get('/hello', (req, res) => {
+    res.send("Hello World!");
+});
 
 app.get('/budget', (req, res) => {
     res.json(budget);
